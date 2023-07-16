0693f0be0c232c2f1ef971b36626d65d78c096ce
Bullets-jyr <jyr@bullets.co.kr>
2023-07-16 11:43:20 +0900

    함수 2 - 중위 함수로 자연스러운 표현

---
+++ src/main/kotlin/E04_InfixFunction.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    infix fun Int.times(str: String) = str.repeat(this)        // 1
+    println(2 times "Bye ")                                    // 2
+//    println(2.times("Bye"))
+
+    val pair = "자바" to "오라클"                                // 3
+//    val pair = "자바".to("오라클")
+    println(pair)
+
+    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    val myPair = "코틀린" onto "젯브레인스"
+    println(myPair)
+
+    val koo = Person("손석구")
+    val yeon = Person("장도연")
+    koo likes yeon                                             // 5
+//    koo.likes(yeon)
+    println(koo.likedPeople.map { it.name })
+}
+
+class Person(val name: String) {
+    val likedPeople = mutableListOf<Person>()
+    infix fun likes(other: Person) { likedPeople.add(other) }  // 6
+}
