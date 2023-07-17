5d804b7c11cfa5a0d6cd649fb6b6acf613ed39bd
Bullets-jyr <jyr@bullets.co.kr>
2023-07-17 15:05:12 +0900

    봉인 클래스

---
+++ src/main/kotlin/E31_SealedClass.kt
@@ -0,0 +1,27 @@
+sealed class Mammal(val name: String)                                          // 1
+//open class Mammal(val name: String)                                          // 1
+
+class Cat(val catName: String) : Mammal(catName)                               // 2
+class Human(val humanName: String, val job: String) : Mammal(humanName)
+
+fun greetMammal(mammal: Mammal): String {
+    when (mammal) {                                                            // 3
+        is Human ->
+            return "안녕하세요, ${mammal.name}님. 직업은 ${mammal.job}이군요."      // 4
+        is Cat ->
+            return "안녕 ${mammal.name}"                                        // 5
+    }                                                                          // 6
+}
+
+fun greetMammalEx2(mammal: Mammal): String =
+    when (mammal) {                                                            // 3
+        is Human ->
+            "안녕하세요, ${mammal.name}님. 직업은 ${mammal.job}이군요."      // 4
+        is Cat ->
+            "안녕 ${mammal.name}"                                        // 5
+//        else -> "새로운 포유류"
+    }                                                                          // 6
+
+fun main() {
+    println(greetMammal(Cat("Snowy")))
+}
