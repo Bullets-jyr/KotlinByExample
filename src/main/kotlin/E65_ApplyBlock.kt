d5bd57c0a0db8b76992fbe4c7a57106366878f5a
Bullets-jyr <jyr@bullets.co.kr>
2023-07-19 10:10:15 +0900

    apply 블록

---
+++ src/main/kotlin/E65_ApplyBlock.kt
@@ -0,0 +1,19 @@
+fun main() {
+    data class Person(var name: String, var age: Int, var about: String) {
+        constructor() : this("", 0, "")
+    }
+    val jake = Person().apply {
+        name = "Jake"
+        age = 30
+        about = "안드로이드 개발자"
+    }                                     // 1
+
+    println(jake.toString())
+
+    val stringDescription = jake.apply {                    // 2
+        name = "Jake"                                       // 3
+        age = 30
+        about = "안드로이드 개발자"
+    }.toString()                                            // 4
+    println(stringDescription)
+}
